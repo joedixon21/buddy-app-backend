bf3e7c4036eb6aa9d415ee6388bf22eeafe9e165
z-t-taylor <157603483+z-t-taylor@users.noreply.github.com>
2024-11-21 09:10:22 +0000

    feat: added to plant schema

--- models/plants.model.js
+++ models/plants.model.js
@@ -5,6 +5,43 @@ const PlantListSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  scientific_name: {
+    type: String,
+    required: true,
+  },
+  other_name: String,
+  cycle: {
+    type: String,
+    required: true,
+  },
+  watering_frequency_in_days: {
+    type: Number,
+    required: true,
+  },
+  sunlight: {
+    type: String,
+    required: true,
+  },
+  default_image: {
+    type: String,
+    required: true,
+  },
+  extra_info: {
+    family: String,
+    type: String,
+    max_height: Number,
+    pruning_month: [String],
+    maintenance: String,
+    soil: String,
+    growth_rate: Number,
+    invasive: Boolean,
+    indoor: Boolean,
+    care_level: String,
+    medicinal: Boolean,
+    poisonous_to_humans: Number,
+    poisonous_to_pets: Number,
+    description: String,
+  },
 });
 
 const Plant = mongoose.model("Plant", PlantListSchema);
