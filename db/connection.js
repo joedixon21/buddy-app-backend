f14a2d95ca2d31c6815c670b7ffe07f6ef2971c3
Joe Dixon <joedixon21@gmail.com>
2024-11-25 11:25:44 +0000

    feat: setting up for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,18 @@
 const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 
+// Determine the environment (default to 'development')
 const ENV = process.env.NODE_ENV || "development";
 
+// Dynamically load the appropriate .env file
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+// Get the MongoDB URI from the environment variables
 const URI = process.env.MONGO_URI;
 
+// Initialize MongoClient with the URI
 const client = new MongoClient(URI);
 
 module.exports = { mongoose, URI, client };
